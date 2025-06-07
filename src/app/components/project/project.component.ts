d1a269bc3456dd3dc963e06f177788b6b39187aa
Ahmet Gürdal <ahmetgurdal32@gmail.com>
2025-06-07 04:32:09 +0300

    Project component, links are added on listing

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { IProject } from '../../pages/projects/projects.component';
 import { ProjectDetailButtonComponent } from '../project-detail-button/project-detail-button.component';
 
@@ -8,19 +15,46 @@ import { ProjectDetailButtonComponent } from '../project-detail-button/project-d
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss',
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
   @Input() project!: IProject;
   @Input() slug!: string;
   @Output() openProjectModal = new EventEmitter<string>();
   @Output() toProjectDetail = new EventEmitter<string>();
 
   isHover = false;
+  isLinksDisplayed = false;
+  isOneColumn = false;
+  isMobile = false;
 
-  onMouseEnter() {
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isOneColumn = event.target.innerWidth < 1050;
+    this.isMobile = event.target.innerWidth < 768;
+  }
+  openVideo() {
     this.isHover = true;
   }
 
-  onMouseLeave() {
+  closeVideo() {
     this.isHover = false;
+  }
+
+  openLinks() {
+    this.isLinksDisplayed = true;
+  }
+
+  closeLinks(event: MouseEvent | TouchEvent) {
+    event.stopPropagation();
+    this.isLinksDisplayed = false;
+  }
+
+  toggleLinks() {
+    this.isLinksDisplayed = !this.isLinksDisplayed;
+    this.isHover = !this.isHover;
+  }
+
+  ngOnInit(): void {
+    this.isOneColumn = window.innerWidth < 1050;
+    this.isMobile = window.innerWidth < 768;
   }
 }
